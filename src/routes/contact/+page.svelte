9d6c5f23ab93e04ba20a8d679ad51c7e6898f548
Gareth Powell <gareth.powell@mosaicisland.co.uk>
2025-09-03 08:59:25 +0100

    Update Contact page: add phone input, adjust form action method, and improve form layout

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -8,16 +8,23 @@
 </script>
 
 <div class="container col-xxl-12 px-4 py-5">
-
+    <form method="post" action="https://forms.un-static.com/forms/3df9195493c0adec60bca6916728620901752777">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div class="col-md-6">
+            <p>
+                For contract availability, reach out directly.
+            </p>
             <div class="mb-3">
                 <label for="contactUsName" class="form-label">Name</label>
-                <input type="email" class="form-control" id="contactUsName" placeholder="Joe Bloggs">
+                <input type="text" class="form-control" id="contactUsName" placeholder="Joe Bloggs">
             </div>
             <div class="mb-3">
                 <label for="contactUsEmail" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="contactUsEmail" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="contactUsPhone" class="form-label">Email address</label>
+                <input type="tel" class="form-control" id="contactUsPhone" placeholder="name@example.com">
             </div>
             <div class="mb-3">
                 <label for="contactUsMessage" class="form-label">Message</label>
@@ -30,9 +37,10 @@
         </div>
         <div class="col-md-6">
             <img src="contact-us.jpg" class="img-fluid rounded-3" alt="Contact Us"/>
+
         </div>
     </div>
-
+    </form>
 </div>
 
 
