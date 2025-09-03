338b6dcae4df027e392d45ab35abbd62d17e72d7
Gareth Powell <gareth.powell@mosaicisland.co.uk>
2025-09-03 12:26:00 +0100

    Refactor Contact page: replace native HTML forms with Sveltestrap components for improved structure and validation

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -4,44 +4,46 @@
 </svelte:head>
 
 <script lang="ts">
-
+    import {Button, Form, FormGroup, Input, Label} from '@sveltestrap/sveltestrap';
+    let validated = false;
 </script>
 
 <div class="container col-xxl-12 px-4 py-5">
-    <form method="post" action="https://forms.un-static.com/forms/3df9195493c0adec60bca6916728620901752777">
+
+
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div class="col-md-6">
             <p>
                 For contract availability, reach out directly.
             </p>
-            <div class="mb-3">
-                <label for="contactUsName" class="form-label">Name</label>
-                <input type="text" class="form-control" id="contactUsName" placeholder="Joe Bloggs">
-            </div>
-            <div class="mb-3">
-                <label for="contactUsEmail" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="contactUsEmail" placeholder="name@example.com">
-            </div>
-            <div class="mb-3">
-                <label for="contactUsPhone" class="form-label">Phone number</label>
-                <input type="tel" class="form-control" id="contactUsPhone" >
-            </div>
-            <div class="mb-3">
-                <label for="contactUsMessage" class="form-label">Message</label>
-                <textarea class="form-control" id="contactUsMessage" rows="10"></textarea>
-            </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">Send</button>
-            </div>
+            <Form action="https://forms.un-static.com/forms/3df9195493c0adec60bca6916728620901752777" method="post"
+                  {validated} on:submit={(e) => e.preventDefault()}>
+                <FormGroup>
+                    <Label for="inputEmail">Email</Label>
+                    <Input type="email" name="email" id="inputEmail" placeholder="joe.bloggs@example.com" required />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="inputName">Name</Label>
+                    <Input type="text" name="name" id="inputName" placeholder="" required/>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="inputPhone">Phone</Label>
+                    <Input type="text" name="phone" id="inputPhone" placeholder=""/>
+                </FormGroup>
+                <FormGroup>
+                    <Label for="inputMessage">Message</Label>
+                    <Input type="textarea" name="message" id="inputMessage" rows="10" placeholder="" required/>
+                </FormGroup>
+                <FormGroup>
+                    <Button class="btn btn-primary" type="submit" on:click={() => (validated = true)}>Send</Button>
+                </FormGroup>
+            </Form>
         </div>
         <div class="col-md-6">
             <img src="contact-us.jpg" class="img-fluid rounded-3" alt="Contact Us"/>
-
         </div>
     </div>
-    </form>
 </div>
-
 
 
 <style>
