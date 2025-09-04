0cdf23eb1c73731ff6e4451f8e48ce2012840d21
Gareth Powell <gareth.powell@mosaicisland.co.uk>
2025-09-04 12:11:05 +0100

    Update CV download link: use `resolve` for dynamic path generation and add new CV file

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -33,7 +33,7 @@
                 — BSc Computer Science with Software Engineering (1999)
             </p>
             <p>
-                <a href={`/cv/ + ${CVFile}`} class="btn btn-info">Download my CV</a>
+                <a href={resolve('/cv/' + CVFile)} class="btn btn-info">Download my CV</a>
             </p>
         </div>
     </div>
