b3b5054d7f03ba15d3449cf09132b1b74336386b
Gareth Powell <gareth.powell@mosaicisland.co.uk>
2025-10-08 12:48:01 +0100

    Update favicon path and clean up commented variables in `+page.svelte`
    - Updated favicon path in `app.html` to use `%sveltekit.assets%` for dynamic asset resolution.
    - Removed unused and commented-out variables in `+page.svelte` for cleaner code.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-    // let opacity = 0.60;
-    // let blur = 24;
-    // let radius = 16;
-
     import Hero from "./content/Hero.svelte";
     import Services from "./content/Services.svelte";
     import Quotes from "./content/Quotes.svelte";
